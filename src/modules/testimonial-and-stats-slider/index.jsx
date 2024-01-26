83ce06c6037e90cb50530116c33a2bf54449749e
aqilusman45 <aqilusman45@gmail.com>
2024-01-26 13:11:41 +0500

    fix: testimonial slider

--- src/modules/testimonial-and-stats-slider/index.jsx
+++ src/modules/testimonial-and-stats-slider/index.jsx
@@ -1,12 +1,12 @@
-import { groq } from "next-sanity";
-import classnames from "classnames";
-import Button from "../../components/button/Button";
-import Heading from "../../components/heading";
-import ReactStars from "react-stars";
-import RichText from "../../components/rich-text";
-import Slider from "../../components/slider";
-import TestimonialStatsCard from "../../components/testimonial-stats-card";
-import Icon from "../../components/icon";
+import { groq } from 'next-sanity'
+import classnames from 'classnames'
+import Button from '../../components/button/Button'
+import Heading from '../../components/heading'
+import ReactStars from 'react-stars'
+import RichText from '../../components/rich-text'
+import Slider from '../../components/slider'
+import TestimonialStatsCard from '../../components/testimonial-stats-card'
+import Icon from '../../components/icon'
 
 export const TESTIMONIAL_AND_STATS_SLIDER = groq`
  statsCards[]{
@@ -15,27 +15,37 @@ export const TESTIMONIAL_AND_STATS_SLIDER = groq`
     sliderCards[]{
       ...,
     }
-`;
+`
 
 function NextArrow(props) {
-  const { onClick } = props;
+  const { onClick } = props
   return (
-    <button className="absolute top-0 right-[41.666667%] z-10" onClick={onClick} >
-
-      <Icon icon="slider-arrow-green" iconHeight={32} iconWidth={32}  otherClasses="hover:scale-110"  />
+    <button
+      className="absolute right-[41.666667%] top-0 z-10"
+      onClick={onClick}
+    >
+      <Icon
+        icon="slider-arrow-green"
+        iconHeight={32}
+        iconWidth={32}
+        otherClasses="hover:scale-110"
+      />
     </button>
-  );
+  )
 }
 
 function PrevArrow(props) {
-  const { onClick } = props;
+  const { onClick } = props
   return (
-    <button className="absolute top-0 right-[37%] z-10" onClick={onClick} >
-
-<Icon icon="slider-arrow-green" otherClasses="rotate-180 hover:scale-110" iconHeight={32} iconWidth={32} />
+    <button className="absolute right-[37%] top-0 z-10" onClick={onClick}>
+      <Icon
+        icon="slider-arrow-green"
+        otherClasses="rotate-180 hover:scale-110"
+        iconHeight={32}
+        iconWidth={32}
+      />
     </button>
-
-  );
+  )
 }
 
 export default function TestimonialAndStatsSlider({
@@ -49,63 +59,65 @@ export default function TestimonialAndStatsSlider({
 }) {
   const testimonialAndStatsSliderClasses = classnames(
     otherClasses,
-    "background-gradient-testimonial-and-stats-slider"
-  );
+    'background-gradient-testimonial-and-stats-slider',
+  )
 
   const settings = {
     speed: 700,
     slidesToShow: 1,
     slidesToScroll: 1,
-    fade:true,
-    dots:true,
+    fade: true,
+    dots: true,
     // autoplay:true,
     // autoplaySpeed:2000,
     nextArrow: <NextArrow />,
-    prevArrow: <PrevArrow />
-  };
+    prevArrow: <PrevArrow />,
+  }
 
   return (
     <section
       className={testimonialAndStatsSliderClasses}
       data-testid="testimonial-and-stats-slider"
     >
-      <div className="max-w-default mx-auto px-4 lg:px-20 xl:px-108 py-20  lg:pt-6 lg:pb-120 flex flex-col gap-6 ">
-        <div className="flex flex-col lg:flex-row justify-between gap-6 items-center">
+      <div className="mx-auto flex max-w-default flex-col gap-6 px-4  py-20 lg:px-20 lg:pb-120 lg:pt-6 xl:px-108 ">
+        <div className="flex flex-col items-center justify-between gap-6 lg:flex-row">
           <Heading
             type="h3"
             otherClasses={classnames(
-              variant === "light" ? "text-blue" : "text-white",
-              "capitalize font-aeronik-pro font-normal w-full lg:max-w-760 xl:max-w-full"
+              variant === 'light' ? 'text-blue' : 'text-white',
+              'capitalize font-aeronik-pro font-normal w-full lg:max-w-760 xl:max-w-full',
             )}
           >
             {heading}
           </Heading>
           <Button {...button} />
         </div>
-        <div className="text-white p-10 mt-10 border-gradient-testimonial-and-stats-slider">
-          <Slider customSettings={settings} customClass="relative" >
+        <div className="border-gradient-testimonial-and-stats-slider mt-10 p-10 text-white">
+          <Slider
+            customSettings={settings}
+            customClass="relative testimonial-slider"
+          >
             {sliderCards?.map((node, i) => {
-              return <TestimonialStatsCard {...node} key={i} />;
+              return <TestimonialStatsCard {...node} key={i} />
             })}
           </Slider>
         </div>
-        <div className="text-white flex w-full flex-col lg:flex-row gap-6 ">
+        <div className="flex w-full flex-col gap-6 text-white lg:flex-row ">
           {Object.keys(testimonial)?.length > 0 && (
-            <div className="w-full lg:w-2/4 p-10 border-gradient-testimonial-and-stats-slider flex flex-col gap-20 lg:gap-0 lg:justify-between">
+            <div className="border-gradient-testimonial-and-stats-slider flex w-full flex-col gap-20 p-10 lg:w-2/4 lg:justify-between lg:gap-0">
               <div className="flex flex-col gap-6">
                 <ReactStars
                   value={testimonial?.rating}
                   count={5}
                   size={20}
-                  color1={"#dfeefd"}
-                  color2={"#ABF57A"}
+                  color1={'#dfeefd'}
+                  color2={'#ABF57A'}
                 />
                 <RichText
                   otherClasses="[&>p]:text-2xl [&>p]:font-aeronik-pro  [&>p]:font-normal [&>p]:capitalize [&>p]:text-white "
                   richText={testimonial?.subText}
                 />
-                <p className="text-16 leading-6 font-normal font-aeronik-pro text-white">
-                  
+                <p className="font-aeronik-pro text-16 font-normal leading-6 text-white">
                   {testimonial?.name}
                 </p>
               </div>
@@ -113,27 +125,27 @@ export default function TestimonialAndStatsSlider({
             </div>
           )}
           {statsCards?.length > 0 && (
-            <div className="w-full lg:w-2/4 p-10 border-gradient-testimonial-and-stats-slider grid grid-cols-2 gap-20 ">
+            <div className="border-gradient-testimonial-and-stats-slider grid w-full grid-cols-2 gap-20 p-10 lg:w-2/4 ">
               {statsCards?.map(({ stats, description }, i) => {
                 return (
                   <div
                     key={stats}
-                    className="text-white flex flex-col gap-6 border-t-[1px] border-t-blue-400"
+                    className="flex flex-col gap-6 border-t-[1px] border-t-blue-400 text-white"
                   >
-                    <p className="h2 capitalize !text-green-300 pt-6">
+                    <p className="h2 pt-6 capitalize !text-green-300">
                       {stats}
                     </p>
 
-                    <p className="text-white font-aeronik-pro text-16 font-normal leading-6 ">
+                    <p className="font-aeronik-pro text-16 font-normal leading-6 text-white ">
                       {description}
                     </p>
                   </div>
-                );
+                )
               })}
             </div>
           )}
         </div>
       </div>
     </section>
-  );
+  )
 }
